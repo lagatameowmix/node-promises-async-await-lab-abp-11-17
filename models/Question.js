b341bd4c13e30fabe3147e6a341808bf5f130225
Sasha V <sashav4@gmail.com>
2017-11-22 04:47:21 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -30,7 +30,7 @@ class Question {
   static Find(id){
     console.log(`Waiting for Question ${id} to load...`)
 
-    resolve new Promise(function(resolve){
+    return new Promise(function(resolve){
       setTimeout(function(){
         const question = Question._All[id-1] // Do Not Edit
         console.log(`...Question ${id} Loaded`) // Do Not Edit
